1bd53f7304ba45ad1065d6834114f5a1d9dab4d8
Gaétan Zwick <gaetan.zwick.dev@gmail.com>
2025-08-08 16:11:53 +0200

    enabled logs, removed useless api vars

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,11 +6,6 @@ import glb from './routes/glb';
 
 // Define environment variable types
 type Bindings = {
-  R2_ACCOUNT_ID: string;
-  R2_ACCESS_KEY_ID: string;
-  R2_SECRET_ACCESS_KEY: string;
-  R2_BUCKET_NAME: string;
-  R2_ENDPOINT: string;
   R2_PUBLIC_ARPENTRY_ENDPOINT: string;
 };
 
