47594f12c71cd12daa7f30470f4f602ef958eb85
Bertil Chapuis <bchapuis@gmail.com>
2025-08-13 23:10:42 +0200

    refactor: remove unused R2 bindings from Bindings type definition

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -8,11 +8,6 @@ import glb from './routes/glb';
  * Shared environment bindings type
  */
 export type Bindings = {
-  R2_ACCOUNT_ID: string;
-  R2_ACCESS_KEY_ID: string;
-  R2_SECRET_ACCESS_KEY: string;
-  R2_BUCKET_NAME: string;
-  R2_ENDPOINT: string;
   R2_PUBLIC_ARPENTRY_ENDPOINT: string;
   KV_ARPENTRY: KVNamespace;
 };
